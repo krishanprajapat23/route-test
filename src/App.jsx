24f0ec90db70af23d524c57f7494b4ae7e47cf8d
Krish <111554369+krishanprajapat23@users.noreply.github.com>
2024-07-19 16:20:02 +0530

    code added

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,17 @@ const App = () => {
                 <nav>
                     <ul>
                         <li>
-                            <Link to="./">Home</Link>
+                            <Link to="/">Home</Link>
                         </li>
                         <li>
-                            <Link to="./about">About</Link>
+                            <Link to="/about">About</Link>
                         </li>
                     </ul>
                 </nav>
 
                 <Routes>
-                    <Route path="./" element={<HomePage />} />
-                    <Route path="./about" element={<AboutPage />} />
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/about" element={<AboutPage />} />
                 </Routes>
             </div>
         </Router>
