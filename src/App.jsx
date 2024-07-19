0f270057eea555c59fd50cf6473a5572085a80ee
Krish <111554369+krishanprajapat23@users.noreply.github.com>
2024-07-19 15:20:55 +0530

    link import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 
